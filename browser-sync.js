85e5f72535388e4662a23a2733d79393cd45c83c
Alexandr Bokov <bok.off@gmail.com>
2023-01-23 18:29:26 +0300

    Закончил верстку хедера

---
+++ browser-sync.js
@@ -0,0 +1,9 @@
+browser-sync start --server --files "index.html,
+pages/index.css,
+vendor/normalize.css,
+vendor/fonts/inter.css,
+blocks/page/page.css,
+blocks/header/header.css,
+blocks/header/__logo-link/header__logo-link.css,
+blocks/header/__logo/header__logo.css,
+blocks/header/__lang/header__lang.css"
